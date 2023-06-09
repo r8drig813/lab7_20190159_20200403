06bfd09661ad213b849998a5b0b950f3e2ce79d3
Rodrigo Tinoco <a20190159@pucp.edu.pe>
2023-06-08 20:33:18 -0500

    Bean jugador

---
+++ proyectoLab7/src/main/java/com/example/proyectolab7/models/beans/jugador.java
@@ -0,0 +1,58 @@
+package com.example.proyectolab7.models.beans;
+
+public class jugador {
+    private int idJugador;
+    private String nombre;
+    private int edad;
+    private String posicion;
+    private String club;
+    private int idSeleccion;
+
+    public int getIdJugador() {
+        return idJugador;
+    }
+
+    public void setIdJugador(int idJugador) {
+        this.idJugador = idJugador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public void setClub(String club) {
+        this.club = club;
+    }
+
+    public int getIdSeleccion() {
+        return idSeleccion;
+    }
+
+    public void setIdSeleccion(int idSeleccion) {
+        this.idSeleccion = idSeleccion;
+    }
+}
---
+++ proyectoLab7/src/main/java/com/example/proyectolab7/models/beans/seleccion.java
@@ -0,0 +1,4 @@
+package com.example.proyectolab7.models.beans;
+
+public class seleccion {
+}
